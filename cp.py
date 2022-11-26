42afd85f8a9be2722e2c0a0306ebbf5388cbf21b
hslau44 <hslau44@gmail.com>
2022-11-26 21:27:15 +0000

    add method - evaluation

--- cp.py
+++ cp.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch 
 from tqdm import tqdm
 from torch.utils.data import Dataset
+import evaluate
 
 
 def get_vocab_dict(data,pad=None):
@@ -38,6 +39,43 @@ def get_phonemes_string(phonemes_detal):
     phonemes_detal = eval(phonemes_detal) if type(phonemes_detal) is not dict else phonemes_detal
     processed_string = char_processing(phonemes_detal['label'])
     return processed_string
+
+
+def evaluation(model,processor,rows,data_dir='data',verbose=True,return_report=False):
+    cer = evaluate.load("cer")
+    audio_inputs = []
+    references = []
+    
+    print('Start extract data')
+    for row in rows:
+        # audio_input
+        fpath = os.path.join(data_dir,row['file_name'])
+        audio_input = sf.read(fpath)[0]
+        audio_inputs.append(audio_input)
+        # labels
+        labels = get_phonemes_string(row['phonetic_detail'])
+        references.append(labels)
+    
+    print('Start Prediction')
+    inputs = processor(audio_inputs, sampling_rate=16_000, return_tensors="pt", padding=True)
+    with torch.no_grad():
+        logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
+    
+    predicted_ids = torch.argmax(logits, axis=-1)      
+    predicted_sentences = processor.batch_decode(predicted_ids)
+    
+    print('Start Evaluation')
+    score = cer.compute(predictions=predicted_sentences, references=references)
+    
+    if return_report:
+        comparision = [{'prediction':pred,'references':ref} for pred,ref in zip(predicted_sentences,references)]
+        result = {'score':score,'comparision':comparision}
+    
+    else:
+        result = score
+    
+    print('Complete')
+    return result
 
 
 class PhonemeSegmentationDataset(Dataset):
