5433d479b538019f754cdfb26c3a9e16f9d2bc0a
hslau44 <hslau44@gmail.com>
2023-04-25 20:11:09 +0000

    add BatchProcessor class for handling batch process

--- data_utils.py
+++ data_utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+import torch
 
 class BaseProcessor(object):
     
@@ -6,3 +8,29 @@ class BaseProcessor(object):
     
     def __call__(self,inputs):
         raise NotImplementedError()
+
+
+class BatchProcessor(BaseProcessor):
+    
+    def __init__(self, to_type=None, from_type=None, **kwargs):
+        self.to_type = to_type
+        self.from_type = from_type
+        super().__init__()
+        
+    def to_batch(self,inputs):
+        if self.to_type == 'pt':
+            inputs = np.concatenate(inputs)
+            inputs = torch.Tensor(inputs)
+        else:
+            raise ValueError(f"Currently not support type: '{self.to_type}'")
+        return inputs
+    
+    def from_batch(self,inputs):
+        length = list(inputs.keys())[0]
+        rows = []
+        for i in range(length):
+            row = {}
+            for key in inputs.keys():
+                row[key] = outputs[key][i]
+            rows.append(row)
+        return rows
