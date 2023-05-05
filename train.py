7176ce72be9d19347a921b3f0e221b6a8a9b67ac
hslau44 <hslau44@gmail.com>
2023-05-05 21:07:59 +0000

    add functions for hyperparameter tuning with raytune

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import torch
 import lightning.pytorch as pl
-from segmentation import nll_loss, avg_sample_acc, SegmentationDataLoader
-from models import CustomWav2Vec2Segmentation
+from ray import tune
+from ray.tune.integration.pytorch_lightning import TuneReportCallback
+from segmentation import nll_loss, avg_sample_acc
 
 
 class PTLModel(pl.LightningModule):
@@ -9,7 +10,6 @@ class PTLModel(pl.LightningModule):
     def __init__(self,model):
         super().__init__()
         self.model = model
-        self.save_hyperparameters()
     
     def training_step(self, batch, batch_idx):
         outputs = self.model(**batch)
@@ -24,6 +24,9 @@ class PTLModel(pl.LightningModule):
         labels = batch.get("labels")
         loss = nll_loss(logits,labels)
         self.log("val_loss", loss)
+        scores = self._compute_metrics(logits,labels)
+        for k,v in scores.items():
+            self.log(f"val_{k}", v)
     
     def test_step(self, batch, batch_idx):
         scores = {}
@@ -37,37 +40,63 @@ class PTLModel(pl.LightningModule):
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
+    
+    def _compute_metrics(self,logits,labels):
+        scores = {}
+        acc = avg_sample_acc(pred,labels)
+        scores.update(acc)
+        return scores 
 
 
-def train(config):
+def raytune_objective(config):
     
-    process_config(config,)
+    # early stopping and mornitoring 
+    # hp
     
-    dataloaders = {}
-    for set_ in ['train','valid']:
-        dataloader_config['dataset_config']['set'] = set_
-        dataloaders[set_] = SegmentationDataLoader(**dataloader_config)
-     
     train_loader = SegmentationDataLoader(_set='train',**config)
     valid_loader = SegmentationDataLoader(_set='dev',**config)
     
     model = CustomWav2Vec2Segmentation(**config)
     ptlmodel = PTLModel(model)
     
-    trainer = pl.Trainer(default_root_dir=os.path.join(output_dir,'logs') )
-    trainer.fit(model, train_loader, valid_loader)
-    return 
-
-
-def test(config):
-
-    dataloader_config['dataset_config']['set'] = 'test'
-    test_loaders = SegmentationDataLoader(**dataloader_config)
+    metrics = {"loss": "ptl/val_loss", "acc": "ptl/val_accuracy"}
     
-    ptlmodel = PTLModel.load_from_checkpoint(os.path.join(output_dir,'logs',"checkpoint.ckpt"))
-    model.eval()
+    trainer = pl.Trainer(
+        max_epochs=config['max_epochs'],
+        gpus=config['num_gpus'],
+        progress_bar_refresh_rate=0,
+        callbacks=[TuneReportCallback(metrics, on="validation_end")]
+    )
     
-    return 
+    trainer.fit(ptlmodel, train_dataloaders=train_loader, val_dataloaders=valid_loader)
+
+
+def raytune_job(config)
+    raytune_config = {}
+    config.update(raytune_config)
+    
+    objective_metric = config['objective_metric']
+    num_samples = config['num_samples']
+    cpu_per_trial = config.get('cpu_per_trial',1)
+    gpu_per_trial = config.get('gpu_per_trial',1)
+
+    analysis = tune.run(
+        objective,
+        resources_per_trial={
+            "cpu": cpu_per_trial,
+            "gpu": gpus_per_trial
+        },
+        metric=objective_metric,
+        mode="max",
+        config=config,
+        num_samples=num_samples,
+        name="tune_job"
+    )
+
+    print(analysis.best_config)
+
+
+
     
 
 
