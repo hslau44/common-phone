4234e1167ad19829028dd5d53bdd7b005669cb62
hslau44 <hslau44@gmail.com>
2023-05-07 19:27:30 +0000

    fix issues

--- sagemaker_entry.py
+++ sagemaker_entry.py
@@ -1,25 +1,22 @@
 import os
 import argparse
-from utils import read_json, add_argument_from_default_config
+from utils import get_default_arg, add_argument_from_default_config
 from segmentation import train
 from hf_train import hyperparameter_optimization
 
 if __name__ == "__main__":
 
-#     config_files = ['segmentation_config.json', 'training_config.json']
-#     configs = [read_json(c) for c in config_files]
+    all_config = get_default_arg()
 
     parser = argparse.ArgumentParser()
 
-#     for config in configs:
-#         add_argument_from_default_config(parser, config)
+    add_argument_from_default_config(parser, all_config, add_as_default=None)
 
     parser.add_argument('--datadir', type=str, default=os.environ['SM_CHANNEL_DATADIR'])
     parser.add_argument("--output_data_dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
     parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
     parser.add_argument("--n_gpus", type=str, default=os.environ["SM_NUM_GPUS"])
-    parser.add_argument("--sm_action", type=str, default='train')
-    parser.add_argument("--mode", type=str, default='debug')
+    parser.add_argument("--sm_action", type=str)
 
     args, _ = parser.parse_known_args()
     input_args = vars(args)
