70e2a8aabfad6ba9928f9250a9818a469c9e2376
hslau44 <hslau44@gmail.com>
2023-05-08 12:19:32 +0000

    use hf_train.objective as sm_action train

--- sagemaker_entry.py
+++ sagemaker_entry.py
@@ -5,8 +5,7 @@ from utils import (
     add_argument_from_default_config,
     process_namespace_arguments,
 )
-# from segmentation import train
-# from hf_train import hyperparameter_optimization
+from hf_train import hyperparameter_optimization, objective
 
 if __name__ == "__main__":
 
@@ -16,10 +15,10 @@ if __name__ == "__main__":
 
     add_argument_from_default_config(parser, all_config)
 
-    # parser.add_argument('--datadir', type=str, default=os.environ['SM_CHANNEL_DATADIR'])
-    # parser.add_argument("--output_data_dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
-    # parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
-    # parser.add_argument("--n_gpus", type=str, default=os.environ["SM_NUM_GPUS"])
+    parser.add_argument('--datadir', type=str, default=os.environ['SM_CHANNEL_DATADIR'])
+    parser.add_argument("--output_data_dir", type=str, default=os.environ["SM_OUTPUT_DATA_DIR"])
+    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"])
+    parser.add_argument("--n_gpus", type=str, default=os.environ["SM_NUM_GPUS"])
     parser.add_argument("--sm_action", type=str)
 
     args, _ = parser.parse_known_args()
@@ -31,12 +30,16 @@ if __name__ == "__main__":
     if args.sm_action == 'train':
         input_args = process_namespace_arguments(args, all_config, True)
         print(f"\n=====\nINPUT ARGUMENTS:\n{input_args}\n======\n")
-        # train(**input_args)
+        scores = objective(input_args)
+        print(f"\n===== SCORES: {scores} =====\n")
+
     elif args.sm_action == 'hyper_optim':
         input_args = process_namespace_arguments(args, all_config, False)
         print(f"\n=====\nINPUT ARGUMENTS:\n{input_args}\n======\n")
-        # hyperparameter_optimization(input_args)
+        hyperparameter_optimization(input_args)
+
     else:
         print(f"\n=====\nINPUT ARGUMENTS:\n{input_args}\n======\n")
         print("No action taken")
+
     print("*****Complete*****")
