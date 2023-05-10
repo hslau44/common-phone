de4966f01932058ac4671483f472dce2fa5ec497
Hok-Shing Lau <60826774+hslau44@users.noreply.github.com>
2023-05-10 15:10:28 +0100

    Update hf_train.py for running the file as a script

--- hf_train.py
+++ hf_train.py
@@ -332,17 +332,37 @@ def hyperparameter_optimization_with_trainer_api(args):
 
 
 if __name__ == "__main__":
-    # local setting
-    args = {}
-    args['mode'] = 'debug'
-    args['datadir'] = 'data'
-    args['output_data_dir'] = 'outputs/exp_05'
-    args['tf32'] = False
-    args['per_device_train_batch_size'] = 2
-    args['per_device_eval_batch_size'] = 2
-    args['num_train_epochs'] = 2
-    args['n_trials'] = 2
     
-    hyperparameter_optimization_with_trainer_api(args)
+    all_config = get_default_arg()
+
+    parser = argparse.ArgumentParser()
+
+    add_argument_from_default_config(parser, all_config)
+
+    parser.add_argument("--output_data_dir", type=str, default='outputs')
+    parser.add_argument("--action", type=str)	
+
+    args, _ = parser.parse_known_args()
+    input_args = process_namespace_arguments(args, all_config)
+
+    print("*****Training locally with HuggingFace*****")
+    print(f"action: {args.action}\ndebug: {args.mode == 'debug'}")
+
+    if args.action == 'train':
+        input_args = process_namespace_arguments(args, all_config, True)
+        print(f"\n=====\nINPUT ARGUMENTS:\n{input_args}\n======\n")
+        scores = objective(input_args)
+        print(f"\n===== SCORES: {scores} =====\n")
+
+    elif args.action == 'hyper_optim':
+        input_args = process_namespace_arguments(args, all_config, False)
+        print(f"\n=====\nINPUT ARGUMENTS:\n{input_args}\n======\n")
+        hyperparameter_optimization_with_trainer_api(input_args)
+
+    else:
+        print(f"\n=====\nINPUT ARGUMENTS:\n{input_args}\n======\n")
+        print("No action taken")
+
+    print("*****Complete*****")
     
     
